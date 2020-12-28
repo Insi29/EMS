cab83962a505d0c6e05402e6dee39909b8029f6e
Arouba29 <76612971+Arouba29@users.noreply.github.com>
2020-12-28 23:28:53 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const responsive={320:{items:1}, 560:{items 2},960:{items:3}}
+
 $(document).ready(function(){
 	$nav=$('.nav');
 	$toggleCollapse=$(.'toggle-Collapse');
@@ -15,4 +17,11 @@ $(document).ready(function(){
 	dots: false,
 	nav: true,
 	navText:[$(.'owl-navigation.owl-nav-prev'),$(.'owl-navigation.owl-nav-next')]
+	responsive:responsive
 });
+$(''.move-up span').click(function () {
+    $('html, body').animate({
+        scrollTop:0
+    },1000);
+})
+
